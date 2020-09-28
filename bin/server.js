1110432f7fc0e2492b8af9dd5fda3e6fd572eb2b
Melvin Carvalho <melvincarvalho@gmail.com>
2020-09-28 19:50:51 +0200

    add root, robots.txt, gitignore, data

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,9 @@ var port = process.env['PORT'] || 80
 
 const extractor = require('unfluff')
 const axios = require('axios')
-const fs = require('fs')
+const fs = require('fs-extra')
+const url = require('url')
+const mkdirp = require('mkdirp')
 
 var uri = ''
 
@@ -16,19 +18,31 @@ fastify.get('/', async (request, reply) => {
   var uri = request.query.uri
 
   if (uri) {
-    reply.type('application/json').code(200)
-
     if (!uri.match(/^http/)) {
       uri = 'https://' + uri
     }
+
+    var parsed = url.parse(uri)
+    console.log('uri', uri)
+    console.log('parsed', parsed)
+    var origin = parsed.hostname
+    console.log('making dir', origin)
+    mkdirp(origin)
+
+    console.log('path')
+
+    reply.type('application/json').code(200)
 
     console.log('extracting', uri)
     var html = await axios.get(uri)
     data = extractor(html.data)
     reply.send(JSON.stringify(data, null, 2))
-    return
 
-    // return data
+    var root = './data'
+    var file =
+      root + '/' + origin + (parsed.pathname === '/' ? '/index.html' : '/')
+    console.log('file', file)
+    fs.outputFile(file, JSON.stringify(data, null, 2))
   } else {
     var index = fs.readFileSync('./index.html')
     reply.type('text/html').code(200)
