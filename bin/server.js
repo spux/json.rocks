811768aafe6df4b3819da4554e33e735048283e7
Melvin Carvalho <melvincarvalho@gmail.com>
2020-09-28 12:12:27 +0200

    first release

---
+++ bin/server.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var port = 3000
+
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.get('/', async (request, reply) => {
+  reply.type('application/json').code(200)
+  return { hello: 'world' }
+})
+
+fastify.listen(port, (err, address) => {
+  if (err) throw err
+  fastify.log.info(`server listening on ${address}`)
+})
