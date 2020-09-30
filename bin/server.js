5313e8beb6c002bf01a7367170e1fd5a47bcc56a
Melvin Carvalho <melvincarvalho@gmail.com>
2020-09-30 18:25:36 +0200

    add content type

--- bin/server.js
+++ bin/server.js
@@ -81,7 +81,8 @@ fastify.get('/', async (request, reply) => {
     }
 
     // response
-    reply.type('text/html').code(200)
+    reply.code(200).header('Content-Type', 'text/html; charset=UTF-8')
+
     var armor = `<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/components/prism-core.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/components/prism-json.min.js"></script>
     <script type="application/ld+json" id="data">
