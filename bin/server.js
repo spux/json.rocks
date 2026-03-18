37cde78eb1a00debb01d0d356e2bcb0f8711ef49
Melvin Carvalho <melvincarvalho@gmail.com>
2026-03-18 16:30:49 +0100

    chore: remove legacy js/jr.js, sort domain panes first in tab order, no-cache panes for dev

--- bin/server.js
+++ bin/server.js
@@ -534,7 +534,7 @@ fastify.get('/panes/:filename', async (request, reply) => {
     reply
       .code(200)
       .header('Content-Type', 'application/javascript')
-      .header('Cache-Control', 'public, max-age=86400')
+      .header('Cache-Control', 'no-cache')
       .send(fileContent)
   } catch (err) {
     reply.code(404).send({ error: 'File not found' })
